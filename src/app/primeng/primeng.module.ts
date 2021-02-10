477606cd077afa711563a68054da774c50967da9
Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>
2021-02-10 01:04:54 -0500

    Configuración Inicial

---
+++ src/app/primeng/primeng.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// MODULOS DE PRIMENG
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+    
+  ],
+  exports: [
+  ]
+})
+export class PrimengModule { }
