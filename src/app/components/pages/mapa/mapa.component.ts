e15f924ee3c151673fae7df5e9a5a179be37d8da
Diego Baes <diegobaesv@gmail.com>
2021-02-13 14:39:23 -0500

    Avanze zoom mapa
    Se implemento sobreescribir el mapa al cambiar el zoom, falta arreglar que se limpie el dibujo anterior, se sobreescribe

--- src/app/components/pages/mapa/mapa.component.ts
+++ src/app/components/pages/mapa/mapa.component.ts
@@ -20,35 +20,71 @@ export class MapaComponent implements OnInit {
     center: L.latLng(47.482019, -1)
   };
 
+
   overlays: any[];
+
+
+  
 
   constructor(private leafletService: LeafletService) { }
 
   ngOnInit(): void {
 
+    
   }
 
   onMapReady(map: L.Map) {
-    this.leafletService.getDepartamentos().subscribe((json: any) => {
+    
+    var ZOOM_DEPARTAMENTO: number = 6;
+    var ZOOM_PROVINCIA: number = 8;
+    var ZOOM_DISTRITO: number = 10;
 
-      /*
-      for (var i = 0; i < json['features'].length; i++) {
+    var jsonDeparmento = null;
+    var jsonProvincia = null;
+    var jsonDistrito = null;
 
-        json['features'][i]['properties']['fill'] = '#00aa22'
-        json['features'][i]['properties']['fill-opacity'] = 0.5
-        json['features'][i]['properties']['stroke'] = '#00aa22'
-        json['features'][i]['properties']['stroke-width'] = '#00aa22'
-        json['features'][i]['properties']['stroke-opacity'] = '#00aa22'
+    this.leafletService.getDepartamentos().subscribe(data =>{
+      jsonDeparmento=data;
+    });
+    this.leafletService.getProvincias().subscribe(data =>{
+      jsonProvincia=data;
+    });
 
-        console.log(json['features'][i])
-        
-      }*/
+    this.leafletService.getDistritos().subscribe(data =>{
+      jsonDistrito=data;
+    });
+
+    map.setView([-9.89, -74.86], 6);
+
+    map.on("zoomend", function (e) {
+      cambioMapa(e);
+    });
 
 
-      L.geoJSON(json, {
+    function cambioMapa(e) {
+      var zoom = e.target._zoom;
+      var jsonMapa = null;
+      var idColorMapa = null;
+      switch (zoom) {
+        case ZOOM_DEPARTAMENTO: 
+          jsonMapa = jsonDeparmento;
+          idColorMapa='DEPA';
+          break;
+        case ZOOM_PROVINCIA:
+          jsonMapa = jsonProvincia;
+          idColorMapa='PROV';
+          break;
+        case ZOOM_DISTRITO:
+          jsonMapa = jsonDistrito;
+          idColorMapa='DIST';
+          break;
+      }
+      console.log(idColorMapa)
+
+      L.geoJSON(jsonMapa, {
         style: function (feature) {
           return {
-            fillColor: getColor(feature.properties.COUNT),
+            fillColor: getColorMapa(feature.properties.COUNT,idColorMapa),
             weight: 2,
             opacity: 1,
             color: 'white',
@@ -56,29 +92,76 @@ export class MapaComponent implements OnInit {
             fillOpacity: 0.7
           };
         }
+
       }).addTo(map);
 
-      function getColor(d) {
-        return d > 1000 ? '#800026' :
-               d > 500  ? '#BD0026' :
-               d > 200  ? '#E31A1C' :
-               d > 100  ? '#FC4E2A' :
-               d > 50   ? '#FD8D3C' :
-               d > 20   ? '#FEB24C' :
-               d > 10   ? '#FED976' :
-                          '#FFEDA0';
+
     }
 
+    function getColorMapa(param, idColor){
+      return getColorDepartamento(param);
+    }
+    
 
-      map.setView([-9.89, -74.86], 6);
+    function getColorDepartamento(d) {
+      return d > 1000 ? '#800026' :
+        d > 500 ? '#BD0026' :
+          d > 200 ? '#E31A1C' :
+            d > 100 ? '#FC4E2A' :
+              d > 50 ? '#FD8D3C' :
+                d > 20 ? '#FEB24C' :
+                  d > 10 ? '#FED976' :
+                    '#FFEDA0';
+    }
 
-    });
+    function getColorProvincia(d) {
+      return d > 1000 ? '#800026' :
+        d > 500 ? '#BD0026' :
+          d > 200 ? '#E31A1C' :
+            d > 100 ? '#FC4E2A' :
+              d > 50 ? '#FD8D3C' :
+                d > 20 ? '#FEB24C' :
+                  d > 10 ? '#FED976' :
+                    '#FFEDA0';
+    }
+
+    function getColorDistrito(d) {
+      return d > 1000 ? '#800026' :
+        d > 500 ? '#BD0026' :
+          d > 200 ? '#E31A1C' :
+            d > 100 ? '#FC4E2A' :
+              d > 50 ? '#FD8D3C' :
+                d > 20 ? '#FEB24C' :
+                  d > 10 ? '#FED976' :
+                    '#FFEDA0';
+    }
 
   }
 
+  loadJsonDepartamentos(): any{
+   
+  }
 
+  loadJsonProvincias(){
+    return this.leafletService.getProvincias().subscribe();
+  }
 
+  loadJsonDistritos(){
+    return this.leafletService.getDistritos().subscribe();
+  }
 
+  /*
+       for (var i = 0; i < json['features'].length; i++) {
+ 
+         json['features'][i]['properties']['fill'] = '#00aa22'
+         json['features'][i]['properties']['fill-opacity'] = 0.5
+         json['features'][i]['properties']['stroke'] = '#00aa22'
+         json['features'][i]['properties']['stroke-width'] = '#00aa22'
+         json['features'][i]['properties']['stroke-opacity'] = '#00aa22'
+ 
+         console.log(json['features'][i])
+         
+       }*/
 
 
 
