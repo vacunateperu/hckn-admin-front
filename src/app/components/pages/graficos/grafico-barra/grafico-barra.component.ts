99be7f2c0c51832116273bfc1f599307cbc9c010
Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>
2021-02-23 02:59:56 -0500

    Ajustes grafico barras
    Signed-off-by: Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>

--- src/app/components/pages/graficos/grafico-barra/grafico-barra.component.ts
+++ src/app/components/pages/graficos/grafico-barra/grafico-barra.component.ts
@@ -8,22 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class GraficoBarraComponent implements OnInit {
 
   data: any;
+  departamentos = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  promedios = [65, 59, 80, 81, 56, 55, 40];
+  deptSeleccionado = 'ANCASH';
+  
 
   constructor() {
     this.data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: this.departamentos,
       datasets: [
           {
-              label: 'My First dataset',
+              label: this.deptSeleccionado,
               backgroundColor: '#42A5F5',
               borderColor: '#1E88E5',
-              data: [65, 59, 80, 81, 56, 55, 40]
-          },
-          {
-              label: 'My Second dataset',
-              backgroundColor: '#9CCC65',
-              borderColor: '#7CB342',
-              data: [28, 48, 40, 19, 86, 27, 90]
+              data: this.promedios
           }
       ]
   }
