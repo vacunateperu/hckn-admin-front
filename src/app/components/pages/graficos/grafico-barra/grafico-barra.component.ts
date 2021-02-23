873c12978437280a71914e404028acf0aabbd29a
Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>
2021-02-23 03:09:04 -0500

    deptSeleccionado
    Signed-off-by: Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>

--- src/app/components/pages/graficos/grafico-barra/grafico-barra.component.ts
+++ src/app/components/pages/graficos/grafico-barra/grafico-barra.component.ts
@@ -10,7 +10,7 @@ export class GraficoBarraComponent implements OnInit {
   data: any;
   departamentos = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   promedios = [65, 59, 80, 81, 56, 55, 40];
-  deptSeleccionado = 'ANCASH';
+  deptSeleccionado = 'Promedio Vulnerabilidad';
   
 
   constructor() {
