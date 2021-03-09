7785467453139e47059dc2ad0249c578c88b9f3d
Diego Baes <diegobaesv@gmail.com>
2021-03-09 13:08:48 -0500

    update grafico tabla

--- src/app/components/pages/graficos/grafico-tabla/grafico-tabla.component.ts
+++ src/app/components/pages/graficos/grafico-tabla/grafico-tabla.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataTable } from 'src/app/models/dataTable';
+import { ComunicacionGraficosService } from 'src/app/services/comunicacion-graficos.service';
+import { EstadisticasService } from 'src/app/services/estadisticas.service';
+import { LeafletService } from 'src/app/services/leaflet.service';
 
 @Component({
   selector: 'app-grafico-tabla',
@@ -10,7 +13,31 @@ export class GraficoTablaComponent implements OnInit {
 
   dataTable: DataTable[];
 
-  constructor() { }
+  constructor(private comunicacionGraficosService: ComunicacionGraficosService,private estadisticasService: EstadisticasService, private leafletService: LeafletService) { 
+    
+    
+    this.comunicacionGraficosService.lugarSeleccionado.subscribe(lugarPadre =>{
+      switch(lugarPadre.tipoLugar){
+        case 'PAIS':
+          console.log('CASE EN '+lugarPadre.tipoLugar)
+          this.estadisticasService.getPromediosDepartamentos().then(departamentos=>{ //CAMBIAR A FUNCION DE TABLA
+            if(departamentos){
+             // this.
+            }
+          })
+          break;
+        case 'DEPARTAMENTO':
+          console.log('CASE EN '+lugarPadre.tipoLugar)
+          break;
+        case 'PROVINCIA':
+          console.log('CASE EN '+lugarPadre.tipoLugar)
+          break;
+        case 'DISTRITO':
+          console.log('CASE EN '+lugarPadre.tipoLugar)
+          break;
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
